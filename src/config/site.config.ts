aceee6a0e77c30b0ef80a05f888da69b2fc79b9f
sunshanqian <sunshanqian@outlook.com>
2025-09-26 23:06:55 +0900

    202509262306

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -5,3 +5,37 @@ type SiteConfig = {
 export const siteConfig: SiteConfig = {
     name: "Fuwari", 
 }
+
+type Link = {
+    name: string
+    url: string
+    external: boolean
+    enabled: boolean
+}
+
+export const NavbarLinks: Link[] = [
+    {
+        name: "Home", 
+        url: "/", 
+        external: false,
+        enabled: true, 
+    }, 
+    {
+        name: "Archive", 
+        url: "/archive", 
+        external: false, 
+        enabled: true, 
+    }, 
+    {
+        name: "About", 
+        url: "/about", 
+        external: false, 
+        enabled: true, 
+    }, 
+    {
+        name: "GitHub", 
+        url: "https://github.com", 
+        external: true, 
+        enabled: true, 
+    }
+]
